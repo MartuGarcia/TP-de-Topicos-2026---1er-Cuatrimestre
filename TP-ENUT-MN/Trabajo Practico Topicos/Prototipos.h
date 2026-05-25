6b0c40a2807e0a98409f9875d9bf129811564e17
Martina Iara Garcia <martugarciaa01@gmail.com>
2026-05-25 20:44:33 -0300

    Add new structures and functions for data handling

--- TP-ENUT-MN/Trabajo Practico Topicos/Prototipos.h
+++ TP-ENUT-MN/Trabajo Practico Topicos/Prototipos.h
@@ -1,25 +1,73 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
-//estructura
+//estructura para punto 1)
 typedef struct
 {
     int region;
     long cantRegistros;
-    double sumaWHOG;
-    double sumaWPER;
+    long sumaWHOG;
+    long sumaWPER;
 }Region;
 
-//funciones de string.h y stdlib.h
+//estructuras para punto 2)
+typedef struct
+{
+    int pos_id;
+    int pos_whog;
+    int pos_wper;
+    int pos_region;
+    int pos_sexo;
+    int pos_edad;
+    int pos_tp_ocupacion;
+    int pos_tp_trabajo;
+    int pos_tp_tnr;
+} PosicionesCampos;
+
+typedef struct
+{
+    long id;
+    long whog;
+    long wper;
+    int region;
+    int sexo;
+    int edad;
+    int tp_ocupacion;
+    int tp_trabajo;
+    int tp_tnr;
+    char grupoEdad[50];
+}Registros;
+
+///vectores y memeoria dinamica
+void* crearVector(size_t capInicial,size_t tamElem);
+void destruirVector(void **vec);
+int redimensionarVector(void **vec, size_t *cap, size_t tamElem); //redimenciono si es necesario
+
+///funciones de string.h y stdlib.h
+char* miStrcpy(char* destino, const char* origen);
 char *miStrtok(char *s1, const char *s2);
+int miStrcmp(const char *s1, const char *s2);
 char* miStrchr(const char *cadena, int caracter);
 int miAtoi(const char *numPtr);
 double miAtof(const char *numPtr);
+void* miMemcpy(void *destino, const void *origen, size_t tam);
 
-// Punto 1)
+/// Punto 1)
 void inicializarRegion(Region *vecRegion);
-void leerArchivo(const char *nombreArchivo);
-int trozarLinea(char *linea,int *region,double *whog, double *wper);
-void calculoPonderadoPorRegion(Region *vec, int region, double whog, double wper);
+void leerArchivoPunto1(const char *nombreArchivo);
+int trozarLineaPunto1(char *linea,int *region,long *whog, long *wper);
+void calculoPonderadoPorRegion(Region *vec, int region, long whog, long wper);
 void mostrarPunto1(Region *vec);
+
+/// Punto 2)
+void leerArchivoPunto2(const char *nombreArchivo);
+void sacarComillasCampos(char *str);
+void obtenerPosicionesCampos(char *linea, PosicionesCampos *pos);
+int trozarLineaPunto2(char *linea, Registros *reg, PosicionesCampos *pos);
+void calcularGrupoEdad(int edad,char *grupoEdad);
+int insertarElementoPunto2(void **vec, size_t *ce, size_t *cap, void *elem, size_t tamElem);
+void mostrarPunto2(void *vec, size_t ce);
+
+
 #endif // FUNCIONES_H_INCLUDED
+
